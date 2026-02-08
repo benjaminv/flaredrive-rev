af8887ffef1672a187bf953feaa3d2bde81aaefa
benjaminv <benvee@gmail.com>
2026-02-09 00:17:12 +1100

    feat: update download functionality in BucketBrowserPage and adjust wrangler configuration

--- frontend-react/pages/BucketBrowserPage.tsx
+++ frontend-react/pages/BucketBrowserPage.tsx
@@ -167,7 +167,10 @@ const BucketBrowserPage = () => {
 
   const handleDownload = (item: StorageListObject) => {
     const url = bucket.getCDNUrl(item)
-    window.open(url, '_blank')
+    const a = document.createElement('a')
+    a.href = url
+    a.download = item.key.split('/').pop() || 'download'
+    a.click()
   }
 
   const handleCopyUrl = async (item: StorageListObject) => {
